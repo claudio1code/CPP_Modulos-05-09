2434373954b9d58a0e38877f34fb9ce3ae06583d
Claudio <claudio1.code@gmail.com>
2026-04-18 21:17:45 -0300

    feat: implement and atribute the member functions em Bureaucrat.cpp

---
+++ cpp_05/Bureaucrat.hpp
@@ -0,0 +1,36 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+
+class Bureaucrat {
+    private:
+        const std::string   _name;
+        int                 _grade;
+        
+    public:
+        Bureaucrat();
+        Bureaucrat(std::string const &name, int grade);
+        Bureaucrat(Bureaucrat const &src);
+        ~Bureaucrat();
+        Bureaucrat &operator=(Bureaucrat const &other);
+
+        std::string         getName() const;
+        int                 getGrade() const;
+
+        void                icrementGrade();
+        void                decrementGrade();
+
+        class GradeTooHighException : public std::exception {
+            public:
+                virtual const char* what() const throw();
+        };
+
+        class GradeTooLowException : public std::exception {
+            public:
+                virtual const char* what() const throw();
+        };
+};
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& obj);
+
+#endif

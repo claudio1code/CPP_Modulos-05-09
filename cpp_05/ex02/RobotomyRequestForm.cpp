84653ecf4437a5cb6a2e607e0813173a3fc2cf92
Claudio <claudio1.code@gmail.com>
2026-04-19 00:34:14 -0300

    --amend

--- cpp_05/ex02/RobotomyRequestForm.cpp
+++ cpp_05/ex02/RobotomyRequestForm.cpp
@@ -6,7 +6,7 @@
 /*   By: claudio <claudio@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/18 23:22:07 by claudio           #+#    #+#             */
-/*   Updated: 2026/04/19 00:08:43 by claudio          ###   ########.fr       */
+/*   Updated: 2026/04/19 00:21:55 by claudio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ RobotomyRequestForm::~RobotomyRequestForm() {}
 
 void RobotomyRequestForm::execute(Bureaucrat const & executor) const {
 	this->checkExecution(executor);
-	std::cout << "AAAAAHHHHHH!!!! IIIIIIIIIIHHHH!!! ELES QUEREM ROUBAR A MINHA MAKITA" << std::endl;
+	std::cout << " RrrrrrRRRrrrRRR! " << std::endl;
 	if (std::rand() % 2 == 0)
 		std::cout << _target << " foi robotizado com sucesso!" << std::endl;
 	else

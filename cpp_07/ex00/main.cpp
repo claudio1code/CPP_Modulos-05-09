42978e35a673286bd85a01da5526a896bf2bdb87
Claudio <claudio1.code@gmail.com>
2026-04-26 23:43:27 -0300

    feat(ex00): implement swap, min, and max templates and add Makefile

--- cpp_07/ex00/main.cpp
+++ cpp_07/ex00/main.cpp
@@ -1,20 +1,19 @@
 #include "whatever.hpp"
-#include <iostream>
-#include <string>
 
-int main()
-{
-	std::string	str1 = "hello";
-	std::string str2 = "world";
-
-	std::cout << str1 << " " << str2 << std::endl;
-	swap(str1, str2);
-	std::cout << str1 << " " << str2 << std::endl;
-
-	int n1 = 42;
-	int n2 = 24;
-	std::cout << n1 << " " << n2 << std::endl;
-	swap(n1, n2);
-	std::cout << n1 << " " << n2 << std::endl;
-
+int main( void ) {
+    int a = 2;
+    int b = 3;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    ::swap( a, b );
+    std::cout <<"Swap: " << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    ::swap(c, d);
+    std::cout << "Swap: " << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
 }

9a3a66c573e1aff640fcbe991698c643ee78a87f
Claudio <claudio1.code@gmail.com>
2026-04-30 01:21:56 -0300

    --amend

--- cpp_09/ex01/main.cpp
+++ cpp_09/ex01/main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        std::cerr << "Error:" << std::endl;
+        std::cerr << "Error" << std::endl;
         return 1;
     }
     try {

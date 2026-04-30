2f7c067e110eda28d1edd714478382b564fcd191
Claudio <claudio1.code@gmail.com>
2026-04-30 01:21:08 -0300

    feat(ex01): replace main.c with main.cpp and implement RPN calculator logic

---
+++ cpp_09/ex01/main.cpp
@@ -0,0 +1,18 @@
+#include "RPN.hpp"
+#include <exception>
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Error:" << std::endl;
+        return 1;
+    }
+    try {
+        RPN claculator;
+        claculator.calculate(argv[1]);
+    } catch (const std::exception& e) {
+        std::cerr << "Error" << std::endl;
+        return 1;
+    }
+        return 0;
+}
